c29d8c1c155a7d8a1e08f566d1b31bccb7bb6b31
Ali Sharaf <ali.charaf59@gmail.com>
2026-02-27 22:52:05 +0400

    Added Tiny note inside FoodType script

--- Cooking In Flame/Assets/ScriptsAly/Ali/NewScripts/FoodType.cs
+++ Cooking In Flame/Assets/ScriptsAly/Ali/NewScripts/FoodType.cs
@@ -1,5 +1,7 @@
 public enum FoodType
 {
+    //THIS SCRIPT IS WHERE I ADD NAMES/LABELS FOR BURGER TYPES ONLY. THE FoodItem script takes the names from here, FoodItem is assigned for eevery Burger, choose the name of the burger there
+
     NormalBurger,
     TomatoBurger,
     PattyOnly
