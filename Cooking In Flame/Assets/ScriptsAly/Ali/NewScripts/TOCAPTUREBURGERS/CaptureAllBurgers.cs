37274d83ab01eb5f358cb2fc54a77ae8cf1dda31
Ali Sharaf <ali.charaf59@gmail.com>
2026-03-02 22:35:07 +0400

    improved burgers design, trying to convert them into sprites

---
+++ Cooking In Flame/Assets/ScriptsAly/Ali/NewScripts/TOCAPTUREBURGERS/CaptureAllBurgers.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.IO;
+
+public class CaptureAllBurgers : MonoBehaviour
+{
+    public Camera cam;
+    public RenderTexture rt;
+    public string fileName = "AllBurgers";
+
+    public void Capture()
+    {
+        RenderTexture currentRT = RenderTexture.active;
+        RenderTexture.active = rt;
+
+        cam.Render();
+
+        Texture2D image = new Texture2D(rt.width, rt.height, TextureFormat.ARGB32, false);
+        image.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        image.Apply();
+
+        byte[] bytes = image.EncodeToPNG();
+        File.WriteAllBytes(Application.dataPath + "/" + fileName + ".png", bytes);
+
+        RenderTexture.active = currentRT;
+
+        Debug.Log("Saved PNG: " + fileName);
+
+
+    }
+
+    void Start()
+    {
+        Capture();
+    }
+}
